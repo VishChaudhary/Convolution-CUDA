eb4761e89f40578d8e5a2afeb8d3afaea6dfb351
Vish Chaudhary <97370632+VishChaudhary@users.noreply.github.com>
2024-05-21 19:24:39 -0400

    Update convolution.cu

--- convolution.cu
+++ convolution.cu
@@ -2,29 +2,46 @@
 #include "math.h"
 
 #define FILTER_RADIUS 2
+#define TILE_DIM 32
 
 //F: Convolution filter array pointer (2D- not linearized)
 __constant__ float F[2*FILTER_RADIUS+1][2*FILTER_RADIUS+1];
 
-__global__ void 2D_convolution_kernel(float *N, float *P, int r, int width, int height){
+__global__ void 2D_convolution_kernel(float *N, float *P,int width, int height){
   //N: Input array pointer (linearlized)
   //P: Output array pointer (linearized)
-  //r: Filter radius (2r+1)
   //width: width of the input/output array
   // height: height of the input/output array
-  int row = blockIdx.y*blockDim.y + threadIdx.y;
-  int col = blockIdx.x * blockDim.x + threadIdx.x;
-  float Pval = 0.0f;
-  for(int rowOffset = 0; rowOffset< 2r+1; rowOffset++){
-    for(int colOffset = 0; colOffset< 2r+1; colOffset++){
-      currRow = row - r + rowOffset;
-      currCol = col - r + colOffset;
-      (if currRow >= 0 && currRow<height && currCol>=0 && currCol <width){
-        Pval+= N[currRow*width+currCol]*F[rowOffset][colOffset];
+
+  int row = blockIdx.y * TILE_DIM + threadIdx.y;
+  int col = blockIdx.x * TILE_DIM + threadIdx.x;
+
+  __shared__ N_s[TILE_DIM][TILE_DIM];
+  if(row<height && col <width){
+    N_s[threadIdx.y][threadIdx.x] = N[row*width+col];
+  }
+  else{
+    N_s[threadIdx.y][threadIdx.x] = 0.0f;
+  }
+  __synchthreads();
+  if(row<height && col<width){
+    float Pval = 0.0f;
+    for(int rowOffset = 0; rowOffset< 2*FILTER_RADIUS+1; rowOffset++){
+      for(int colOffset = 0; colOffset< 2*FILTER_RADIUS+1; colOffset++){
+        if(threadIdx.y-FILTER_RADIUS+rowOffset>=0 && threadIdx.y-FILTER_RADIUS+rowOffset < TILE_DIM && threadIdx.x-FILTER_RADIUS+colOffset >=0 && threadIdx.x-FILTER_RADIUS+colOffset < TILE_DIM){
+          Pval+= F[rowOffset][colOffset]*N_s[threadIdx.y-FILTER_RADIUS+rowOffset][threadIdx.x-FILTER_RADIUS+colOffset];
+        }
+        else{
+          currRow = row - FILTER_RADIUS + rowOffset;
+          currCol = col - FILTER_RADIUS + colOffset;
+          if (currRow >= 0 && currRow<height && currCol>=0 && currCol <width){
+            Pval+= N[currRow*width+currCol]*F[rowOffset][colOffset];
+          }
+        }
       }
     }
+    P[row*width+col] = Pval;
   }
-  P[row*width+col] = Pval;
 } 
 
 
